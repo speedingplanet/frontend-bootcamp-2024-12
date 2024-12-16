9f08e3d3a04c59ed1010b9ea91bd73e106da66dc
John Paxton <johnpaxton@gmail.com>
2024-12-16 14:44:51 -0500

    Calculator start

--- react-labs/react-app/src/calculator/CalculatorApp.tsx
+++ react-labs/react-app/src/calculator/CalculatorApp.tsx
@@ -1,3 +1,28 @@
+import React from 'react';
+import './calculator.css';
+import CalculatorButton from './CalculatorButton';
+
 export default function CalculatorApp() {
-	return <h2>Intuit Calculator</h2>;
+	let firstName = 'John';
+
+	return (
+		<>
+			<h2>{firstName}'s Calculator</h2>
+			{/* div.calculator>button{$}*9 */}
+			<div className="calculator">
+				{/* We'll come back to this in the afternoon */}
+				{/* <CalculatorButton>1</CalculatorButton> */}
+				{/* CalculatorButton[display="$"]*9 */}
+				<CalculatorButton display="1"></CalculatorButton>
+				<CalculatorButton display="2"></CalculatorButton>
+				<CalculatorButton display="3"></CalculatorButton>
+				<CalculatorButton display="4"></CalculatorButton>
+				<CalculatorButton display="5"></CalculatorButton>
+				<CalculatorButton display="6"></CalculatorButton>
+				<CalculatorButton display="7"></CalculatorButton>
+				<CalculatorButton display="8"></CalculatorButton>
+				<CalculatorButton display="9"></CalculatorButton>
+			</div>
+		</>
+	);
 }
---
+++ react-labs/react-app/src/calculator/CalculatorButton.tsx
@@ -0,0 +1,21 @@
+
+/*
+export: Make this function available for import elsewhere
+default: import without curly braces, e.g.
+	import CalculatorButton from './CalculatorButton'
+	instead of 
+	import { CalculatorButton } from './CalculatorButton'
+	ONLY ONE default export per file
+	This is mostly a React convention, no real benefit or penalty
+function: JS keyword for defining a function
+CalculatorButton: The name of our function
+*/
+
+interface Props {
+	display: string;
+}
+
+export default function CalculatorButton({display}: Props) {
+	return <button type="button">{display}</button>;
+}
+
