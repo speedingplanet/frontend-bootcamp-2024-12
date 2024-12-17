d8ca0d85b25704fd29299c6aa302ab03e61e01db
John Paxton <johnpaxton@gmail.com>
2024-12-17 11:06:37 -0500

    Add Jest testing config

---
+++ react-demos/jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+	testMatch: [ "**/__tests__/**/*.[jt]s?(x)"],
+	moduleNameMapper: {
+		// Suggested here: https://stackoverflow.com/a/54117206
+		// This is a bad solution which Jest should be criticized for
+		'^lodash-es$': 'lodash',
+
+		// Mock out imports of CSS files
+		// see https://jestjs.io/docs/webpack#handling-static-assets
+		'\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+	},
+};
---
+++ react-demos/src/__tests__/basic.jest.ts
@@ -0,0 +1,5 @@
+describe('A suite of tests', () => {
+	it('should add two numbers', () => {
+		expect(2 + 4).toBe(6);
+	});
+});
