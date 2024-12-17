a8b7155b19a7d7928a1feef03bd0bdf872ea71b3
John Paxton <johnpaxton@gmail.com>
2024-12-17 18:22:42 -0500

    End of Tuesday

--- react-labs/react-app/src/calculator/CalculatorDisplay.tsx
+++ react-labs/react-app/src/calculator/CalculatorDisplay.tsx
@@ -7,7 +7,7 @@ export default function CalculatorDisplay({ displayValue }: Props) {
 	return (
 		<div className="display">
 			{/* Print displayValue, unless it is null or undefined, then print zero */}
-			<div>{displayValue ?? 0}</div>
+			<div data-testid="display">{displayValue ?? 0}</div>
 		</div>
 	);
 }
--- react-labs/react-app/src/calculator/__tests__/CalculatorApp.jest.tsx
+++ react-labs/react-app/src/calculator/__tests__/CalculatorApp.jest.tsx
@@ -1,16 +1,17 @@
 import CalculatorApp from '../CalculatorApp';
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 it('should render the calculator', () => {
 	render(<CalculatorApp />);
 
 	// let clearButton = screen.getByText('C');
-	let clearButton = screen.getByRole('button', {name: 'C'});
+	let clearButton = screen.getByRole('button', { name: 'C' });
 	expect(clearButton).toBeInTheDocument();
 });
 
-it('should clear the initial display when the "C" button is clicked', () => {
+it('should clear the initial display when the "C" button is clicked', async () => {
 	/*
 	Render CalculatorApp
 	Check the display value (how can we get that?), what should it be?
@@ -18,9 +19,20 @@ it('should clear the initial display when the "C" button is clicked', () => {
 	Click on the clear button
 	Check the display value, what should it be now?
 	*/
+
+	render(<CalculatorApp />);
+	let user = userEvent.setup();
+
+	// TODO: Maybe refactor to use an appropriate ARIA role for the display?
+	let displayDiv = screen.getByTestId('display');
+	expect(displayDiv.textContent).toBe('0');
+	let clearButton = screen.getByRole('button', {name: 'C'});
+	await user.click(clearButton);
+	expect(displayDiv.textContent).toBe('');
+
 });
 
-it('should update the display value when number keys are clicked', () => {
+it('should update the display value when number keys are clicked', async  () => {
 	/*
 	Render CalculatorApp
 	Check the display value (how can we get that?), what should it be?
@@ -31,8 +43,19 @@ it('should update the display value when number keys are clicked', () => {
 	Click it
 	See if the display value updated
 	*/
-});
 
+	render(<CalculatorApp />);
+	let user = userEvent.setup();
+	let displayDiv = screen.getByTestId('display');
+	expect(displayDiv.textContent).toBe('0');
+	let sevenButton = screen.getByRole('button', {name: '7'});
+	let fiveButton = screen.getByRole('button', {name: '5'});
+	await user.click(fiveButton);
+	expect(displayDiv.textContent).toBe('05');
+	await user.click(sevenButton);
+	expect(displayDiv.textContent).toBe('057');
+
+});
 
 it('should clear the display after the display has been updated, when the "C" button is clicked', () => {
 	/*
