7ae0934a81ad8b4a96eb58a8f6c7289c36dff15d
John Paxton <johnpaxton@gmail.com>
2024-12-17 17:18:43 -0500

    User event exercise

--- react-labs/react-app/src/calculator/__tests__/CalculatorApp.jest.tsx
+++ react-labs/react-app/src/calculator/__tests__/CalculatorApp.jest.tsx
@@ -9,3 +9,43 @@ it('should render the calculator', () => {
 	let clearButton = screen.getByRole('button', {name: 'C'});
 	expect(clearButton).toBeInTheDocument();
 });
+
+it('should clear the initial display when the "C" button is clicked', () => {
+	/*
+	Render CalculatorApp
+	Check the display value (how can we get that?), what should it be?
+	Get a reference to the Clear button
+	Click on the clear button
+	Check the display value, what should it be now?
+	*/
+});
+
+it('should update the display value when number keys are clicked', () => {
+	/*
+	Render CalculatorApp
+	Check the display value (how can we get that?), what should it be?
+	Get a reference to a number button
+	Click it
+	See if the display value updated
+	Get a reference to a different number button
+	Click it
+	See if the display value updated
+	*/
+});
+
+
+it('should clear the display after the display has been updated, when the "C" button is clicked', () => {
+	/*
+	Render CalculatorApp
+	Check the display value (how can we get that?), what should it be?
+	Get a reference to a number button
+	Click it
+	See if the display value updated
+	Get a reference to a different number button
+	Click it
+	See if the display value updated
+	Get a reference to the Clear button
+	Click on the clear button
+	Check the display value, what should it be now?
+	*/
+});
