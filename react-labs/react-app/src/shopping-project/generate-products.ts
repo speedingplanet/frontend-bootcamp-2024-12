158386ac83b089cf418025e8d4bbc35de35ce3f2
John Paxton <johnpaxton@gmail.com>
2024-12-18 17:50:51 -0500

    Generate fake products

---
+++ react-labs/react-app/src/shopping-project/generate-products.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker';
+import { Product } from './shopping-types';
+
+export function generateProduct(): Product {
+	return {
+		name: faker.commerce.productName(),
+		price: Number(faker.commerce.price()),
+		description: faker.commerce.productDescription(),
+		material: faker.commerce.productMaterial(),
+		department: faker.commerce.department(),
+		id: faker.string.alphanumeric({ casing: 'upper', length: 10 }),
+		rating: faker.number.float({ min: 1.0, max: 5.0, fractionDigits: 1 }),
+	};
+}
+
+export function generateProducts(count = 1): Array<Product> {
+	let products = new Array(count);
+	for (let x = 0; x < count; x ++) {
+		products[x] = generateProduct();
+	}
+
+	return products;
+}
