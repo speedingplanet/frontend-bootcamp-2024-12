718bbed32151df330b8f3d5010c2d6016eebbd55
John Paxton <johnpaxton@gmail.com>
2024-12-18 16:55:26 -0500

    Added types

---
+++ react-labs/react-app/src/shopping-project/shopping-types.ts
@@ -0,0 +1,25 @@
+export interface Product {
+	id: string;
+	name: string;
+	price: number;
+	description: string;
+	material: string;
+	department: string;
+	rating: number
+}
+
+export interface Cart {
+	id: string;
+	customerId: string;
+	cartStatus: CartStatus;
+	isEmpty: boolean;
+	products: CartItem[];
+	totalPrice: number;
+}
+
+export interface CartItem {
+	productId: string;
+	quantity: number;
+}
+
+export type CartStatus = 'Pending' | 'Saved' | 'Checked-out';
