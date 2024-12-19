b3533c85f41d96c7d2998354ac362616b4ecdf6d
John Paxton <johnpaxton@gmail.com>
2024-12-19 16:43:24 -0500

    CSS Grid

--- react-labs/react-app/src/shopping-project/ProductBrowser.tsx
+++ react-labs/react-app/src/shopping-project/ProductBrowser.tsx
@@ -1,59 +1,34 @@
 import React from 'react';
 import { generateProducts } from './generate-products';
 
-/*
-Homework:
-
-Turn this in an HTML table. (Or use <div>s and a CSS grid)
-Show the product name, price, material, rating
-Hints: The table header should be static
-The table rows in the table body correspond to each product
-The table data cells in each row correspond to the properties of the product
-
-eventually looks something like:
-
-<table>
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th>Price</th>
-			<th>Material</th>
-			<th>Rating</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td>Licensed Wooden Soap</td>
-			<td>10.00</td>
-			<td>Wood</td>
-			<td>3.2</td>
-		</tr>
-		<tr>
-			<td>Modern Rubber Gloves</td>
-			<td>312.42</td>
-			<td>Rubber</td>
-			<td>4.7</td>
-		</tr>
-	</tbody>
-</table>
-
-
-*/
-
-
-
 export const ProductBrowser = () => {
-	let products = generateProducts(10);
+	let products = generateProducts(200);
 	return (
 		<>
 			<h3>Product Browser</h3>
-			<div>
-				<ul>
-					{products.map((product) => (
-						<li key={product.id}>{product.name}</li>
-					))}
-				</ul>
+			<div className="shopping-browser">
+				<div className="browser-header-row">
+					<div>Name</div>
+					<div>Price</div>
+					<div>Material</div>
+					<div>Department</div>
+				</div>
+				{products.map((product) => (
+					<div
+						className="browser-body-row"
+						key={product.id}
+					>
+						<div>{product.name}</div>
+						<div>{product.price}</div>
+						<div>{product.material}</div>
+						<div>{product.department}</div>
+					</div>
+				))}
 			</div>
 		</>
 	);
 };
+
+/*
+
+*/
